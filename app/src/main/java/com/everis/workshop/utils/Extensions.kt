b2db540b4622aeecf256cc5e2f438b4f3e4b851b
Raul Illan Garcia <rillanga@BCN-Raul-Android.local>
2019-09-24 09:40:22 +0200

    added extension class

--- app/src/main/java/com/everis/workshop/utils/Extensions.kt
+++ app/src/main/java/com/everis/workshop/utils/Extensions.kt
@@ -1,8 +1,21 @@
 package com.everis.workshop.utils
 
+import android.app.Activity
 import android.content.Context
+import android.location.Location
+import android.location.LocationManager
 import android.widget.Toast
 
 fun Context.toast(message: CharSequence, duration: Int = Toast.LENGTH_LONG) {
     Toast.makeText(this, message, duration).show()
+}
+
+fun Location.isLocationEnabled(activity: Activity): Boolean {
+    val locationManager = activity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+}
+
+fun <T> List<T>.ordenar(t: T) {
+
 }
